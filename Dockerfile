043a449a26e31dd622bed0d62dcc3341525cb72d
Arsène Thieffry <arsene.thieffry@gmail.com>
2019-11-18 19:32:03 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:latest
+
+RUN mkdir -p /bot
+WORKDIR /bot
+
+COPY package.json /bot
+RUN npm install
+
+COPY . /bot
+
+CMD ["npm", "start"]
---
+++ app.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+const prefix = '!';
+
+client.on('message', message => {
+
+    if (message.author.bot) return;
+    if (message.content.indexOf(prefix) !== 0) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'octogone') {
+        let players = message.mentions.members;
+        players.set(message.member.user.id, message.member);
+
+        let winner = players.random();
+
+        return message.channel.send(`${winner.nickname || winner.user.username} won!`)
+    }
+});
+
+client.login(process.env.TOKEN);
