ce03c39501497ee1bbf2603679648cb39213a3f8
Arsène Thieffry <arsene.thieffry@gmail.com>
2021-03-29 21:10:42 +0200

    Simplify datetime formatter

--- irloctogone.js
+++ irloctogone.js
@@ -1,5 +1,3 @@
-const days = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
-const months = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
 const places = ['Gare du Nord (Paris)', 'Place Saint Pierre (Toulouse)', 'Chez Zekth', 'Part-Dieu (Lyon)'];
 function rnJesus(max) {
     return Math.floor(Math.random() * Math.floor(max))
@@ -9,6 +7,6 @@ export default function irloctogone() {
   const date = new Date();
   date.setDate(date.getDate() + rnJesus(5));
   date.setTime(date.getTime() + (rnJesus(8)*60*60*1000));
-  
-  return `Le ${days[date.getDay()]} ${date.getDate()} ${months[date.getMonth()]}, ${places[rnJesus(places.length)]}`
+
+  return `Le ${new Intl.DateTimeFormat('fr-FR', { dateStyle: 'full', timeStyle: 'short', timeZone: 'Europe/Paris' }).format(date)}, ${places[rnJesus(places.length)]}`
 }
